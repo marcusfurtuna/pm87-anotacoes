1925fcf02dce09ddbf8f1f2fc093786beaa33679
marcusfurtuna <45171446+marcusfurtuna@users.noreply.github.com>
2018-11-19 11:46:32 -0200

    Update TesteGit.java

--- TesteGit.java
+++ TesteGit.java
@@ -1,11 +1,10 @@
 public class TesteGit{
 
-public static void main (String [] args){
+    public static void main (String [] args){
 
+  System.out.println("Exemplo de teste de uso do git");
 
-System.out.println("Exemplo de teste de uso do git");
-
-}
+    }
 
 
 }
